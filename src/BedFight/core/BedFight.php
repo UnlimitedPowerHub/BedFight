e4476c2f2bc6a1c914deaab815362499306571ad
AmirrezaJalilian <TheCleverMr@gmail.com>
2026-09-13 04:23:38 +0330

    fix: Initialize services in correct dependency order
    - LeaderboardManager before GameManager (GameManager accesses it in constructor)
    - NPCManager before GameManager (GameManager accesses it in constructor)
    - BotManager created early, setGameManager() called after GameManager creation

--- src/BedFight/core/BedFight.php
+++ src/BedFight/core/BedFight.php
@@ -66,10 +66,10 @@ class BedFight extends PluginBase {
 
         $this->arenaManager = new ArenaManager($this, $this->storage, $this->config);
         $this->botManager = new BotManager($this, null, $this->config);
-        $this->gameManager = new GameManager($this, $this->arenaManager, $this->config);
-        $this->botManager->setGameManager($this->gameManager);
         $this->leaderboardManager = new LeaderboardManager($this, $this->storage, $this->config);
         $this->npcManager = new NPCManager($this, $this->config);
+        $this->gameManager = new GameManager($this, $this->arenaManager, $this->config);
+        $this->botManager->setGameManager($this->gameManager);
         $this->formManager = new \BedFight\Form\FormManager($this);
         $this->eventListener = new \BedFight\Event\EventListener($this);
 
